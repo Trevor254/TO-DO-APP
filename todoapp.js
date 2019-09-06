8de00f323e3da3437d8d99fd832e0c7b752e01dc
Trevor254 <trevoromondi607@gmail.com>
2019-09-06 09:43:11 +0300

    changed index title

--- todoapp.js
+++ todoapp.js
@@ -18,37 +18,79 @@ const todos =[{
 }];
 
 
-const filters={
-    searchText:""
+const filters={   
+    searchText:"",
+    hideCompleted:false
 };
 
 
+
 const renderTodos = function(todos, filters) {
-    const incompleteTodos = todos.filter(function(todo){
+
+    const filteredTodos = todos.filter(function(todo){
+        const searchTextMatch = todo.text.toUpperCase().includes
+        (filters.searchText.toUpperCase())
+        const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
+
+        return searchTextMatch && hideCompletedMatch;
+    });
+
+    const incompleteTodos = filteredTodos.filter(function(todo){
         return !todo.completed;
-    })
+    });
 
-const summary = document.createElement('h2');
-summary.textContent = `You have ${incompleteTodos.length} todos left`;
-document.querySelector('body').appendChild(summary);
+document.querySelector('#todos').innerHTML = "";
 
-todos.forEach(function(todo){
+
+const summary = document.createElement('h2');  //document.create element creates a new element in the form of h2
+summary.textContent = `You have ${incompleteTodos.length} todos left`; //
+document.querySelector('#todos').appendChild(summary);
+
+
+filteredTodos.forEach(function(todo){
     const p = document.createElement('p');
     p.textContent = todo.text;
-    document.querySelector('body').appendChild(p);
- })
+    document.querySelector('#todos').appendChild(p);
+})
 
-}
+};
+
+renderTodos(todos,filters);
 
 
 
-// listen for new todo creation
-document.querySelector('#add-todo').addEventListener('click',function(e){
-    console.log('Add a new todo');
-});
+
 
 //Listen for to-do text change
 document.querySelector('#new-todo-text').addEventListener('input', function(e){
     console.log(e.target.value);
 })
 
+
+
+
+//search for todos
+document.querySelector('#search-text').addEventListener('input',function(e){
+    filters.searchText = e.target.value;
+    renderTodos(todos,filters);
+})
+
+
+
+//add todo to DOM
+document.querySelector('#new-todo').addEventListener('submit', function(e){
+    e.preventDefault();
+    todos.push({
+        text: e.target.elements.todoText.value,
+        completed: false
+    });
+    
+    renderTodos(todos, filters);
+    e.target.elements.todoText.value = ''
+});
+
+
+document.querySelector('#hide-completed').addEventListener('change', function(e){
+    filters.hideCompleted = e.target.checked;
+    renderTodos(todos, filters);
+})
